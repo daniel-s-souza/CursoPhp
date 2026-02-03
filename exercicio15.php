deff1e0b72ecc75189d22b0c13328673cfc106d2
Daniel <danielsantsouza94@gmail.comm>
2026-02-03 09:06:39 -0300

    exercicio15 focado no uso de operador de igualdade

---
+++ exercicio15.php
@@ -0,0 +1,22 @@
+<?php 
+
+## Exercicio para estudo do operador de igualdade (==) ##
+
+$valor1 = 100;
+$valor2 = "100";  
+
+if($valor1 == $valor2){
+    echo "Os valores são iguais";
+} else {
+    echo "Os valores são diferentes";
+}
+
+
+
+
+
+?>
+
+
+
+
