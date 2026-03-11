95142ae61504a39841caf4271bca07cbe119e792
Daniel <danielsantsouza94@gmail.comm>
2026-03-11 10:26:51 -0300

    resolve exercicios propostos na aula

---
+++ sec6exerc1.php
@@ -0,0 +1,27 @@
+<?php
+
+$a = 5;
+$b = 2;
+
+if($a > $b){
+    echo "O valor de a é maior que o valor de b <br>";
+}
+
+if($b > $a){
+    echo "O valor de b é maior que o valor de a <br>";
+}
+
+$c = "Matheus";
+$d = "Pedro";
+
+if($c != $d){
+    echo "O valor de c é diferente do valor de d <br>";
+}
+
+$e = 12;
+$f = 11;
+
+if ($e >= $f){
+    echo "O valor de e é maior ou igual ao valor de f <br>";
+}
+?>
