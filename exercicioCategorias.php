8ee5d96fbf8fc035b1ad0fdf11b5574028700904
Daniel <danielsantsouza94@gmail.comm>
2026-03-11 11:55:20 -0300

    Exercicio realizado na plataforma Udemy, apesar de nao ter instruções de qual metodo usar para resolver, decidi para fins de treino usar a estrutura de comparação switch case

---
+++ exercicioCategorias.php
@@ -0,0 +1,23 @@
+<?php
+
+function verificarCategoria ($categoria) {
+
+    switch ($categoria) {
+        case "eletrônicos":
+        return "Essa categoria é de produtos eletrônicos";
+        break;
+        case "vestuário":
+        return "Essa categoria é de produtos de vestuário";
+        break;
+        case "alimentos":
+        return "Essa categoria é de produtos alimentícios";
+        break;
+        
+        default:
+            return "Categoria desconhecida";
+    }
+
+
+
+
+}
